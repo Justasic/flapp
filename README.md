2f1342f82cd8829b207402ed0c3cb23b2850247f
Justin Crawford <Justin@stacksmash.net>
2021-05-19 21:02:55 -0700

    Initial Commit with initial implementation

---
+++ README.md
@@ -0,0 +1,131 @@
+# Flapp
+
+Flapp is the ultimate translation system for your project. It is intended to correctly
+translate all strings in a program to any language without needing to add multiple
+strings for the same sentence just to make something plural. This also allows for
+locale-specific dates and times, pluralization, and potentially even conditionals
+which enables advanced formatting!
+
+Localization is nothing more than a string lookup system from a set of files which
+you can specify the system for looking up those file names. The translate function
+takes a string which contains a sequence of filters inside the translatable string.
+These `filters` are similar to [Django template filters](https://docs.djangoproject.com/en/3.2/topics/templates/#filters)
+that allow for flexible string generation for translation.
+
+Syntax for all filters is as follows:
+`{VARIABLE|filter:filter "arguments"}`
+, the variable names are case insensitive.
+
+## Setup
+```bash
+python setup.py install
+```
+
+## Usage
+
+Assume there is a file `translations.en_US.yml` in the folder `locales` which exists with the content:
+```yaml
+user:
+    hello-message: "Hello, {USERNAME|title}! It is {NOW|date:\"D d M Y\"}!"
+```
+and `translations.ru_RU.yml` in the same folder:
+```yaml
+user:
+    hello-message: "Привет, {USERNAME|title}! Сегодня {NOW|date:\"D d M Y\"}!"
+```
+
+To format/translate a string:
+```python
+>> from flapp import Flapp
+>> from datetime import datetime
+>> translator = Flapp("/path/to/locales", file_pattern="translations.{locale}.yml", default="en_US")
+>> translator.translate("user.hello-message", username="flapper", now=datetime.now())
+Hello, Flapper! It is Wed 09 Jan 2008!
+>> translator.translate("user.hello-message", "ru_RU", username="flapper", now=datetime.now())
+Привет, Flapper! Сегодня Wed 09 Jan 2008!
+```
+
+The translate function takes a "node" which represents the path to the string. See [Loading other formats](#Loading-other-formats) below.
+
+The second argument to `translate` is used to specify a locale different from the default locale, this locale is used to find and load the
+locale file if it is not already loaded.
+
+## Built-in filters
+
+There are some basic filters that come included:
+
+| filter | Description | Arguments | Default |
+| ----------- | ----------- | ----------- | ----------- |
+| `pluralize` | Add text if a number is greater than 1 | comma separated list based on count | "s" if plural |
+| `yesno` | Convert a boolean value to the language's boolean representation | comma separated pair in place of "yes,no" | "yes" or "no" |
+| `datetime` | Format the variable as a datetime string | strftime-compatible format string | datetime default formatt |
+| `cut` | Removes text from a variable | quoted string of text to remove | *N/A* |
+| `empty_if_false` | Returns an empty string if the variable evaluates to False | value returned if True | *N/A* |
+| `empty_if_true` | Returns an empty string if the variable evaluates to True | value returned if False | *N/A* |
+| `default_if_none` | Returns a default value if the variable is False/None | default value | *N/A* |
+| `lower` | Convert the variable to all lowercase | *N/A* | *N/A* |
+| `upper` | Convert the variable to all uppercase | *N/A* | *N/A* |
+| `title` | Convert the variable to titlecase (capitalize the first letter of every word) | *N/A* | *N/A* |
+| `join` | Join a list together with a deliminator | deliminator string | ", " |
+
+You can also add your own filters if you need to do some special formatting. The `variable` to be formatted and the filter's arguments
+are passed as strings to the bound function. An example:
+```yaml
+user:
+    hello-message: "Hello {WHO|memecase}!"
+```
+
+```python
+>> from flapp import Flapp
+>> from random import choice
+>> translator = Flapp("/path/to/locales", identifier="translations.{locale}.yml", default="en_US")
+>> def memecase(variable, argument: str):
+>>    return ''.join(choice((str.upper, str.lower))(c) for c in str(variable))
+>> translator.add_filter('memecase', memecase)
+>> translator.translate("user.hello-message", who="World")
+Hello WoRLd!
+```
+
+Note that the type of `variable` in the `memecase` function is undefined as it leaves it up to the `memecase` function
+to return a correctly formatted string. Any data returned from the function will be cast as a string regardless.
+
+# Loading other formats
+
+While Yaml and JSON files are supported by default, you can load the same
+data from other sources by calling `add_locale` with a dictionary to that
+of what `json.loads()` would return. The node can be deliniated by a period (`.`)
+and each part of the node is then used as the key for the recursive dictionaries.
+An example from the above example code effectively does:
+
+```python
+>> yamldata['user']['hello-message']
+Hello, {USERNAME|title}! It is {NOW|date:\"D d M Y\"}!
+```
+
+You can verify if a locale is loaded using `locale_loaded("en_GB")` and you can unload
+loaded locales with `remove_locale("en_GB")` (replacing "en_GB" with the locale format you're using)
+
+---
+
+## License
+Copyright *2021 Justin Crawford <Justin@stacksmash.net>*
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without 
+restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or 
+sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
---
+++ flapp/__init__.py
@@ -0,0 +1,191 @@
+"""
+Copyright 2021 Justin Crawford <Justin@stacksmash.net>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without 
+restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or 
+sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+import datetime
+import time
+import typing
+import json
+import yaml
+import warnings
+from pathlib import Path
+
+class Flapp:
+	_loaded_locales = {}
+
+	def _pluralize(self, var, arg: str) -> str:
+		singular = ""
+		plural = "s" if not arg else arg
+		if "," in arg:
+			singular, plural = arg.split(",", 1)
+
+		# TODO: support locale-specific plurals like
+		# 2 or 3 being different in some languages
+		return singular if int(var) == 1 else plural
+
+	def _yesno(self, var, arg: str) -> str:
+		if not arg:
+			return "yes" if var else "no"
+		elif "," in arg:
+			return arg.split(",")[int(bool(var))]
+
+	def _datetime(self, var, arg: str) -> str:
+		# TODO: Support locale-specific date formatting?
+		if isinstance(var, datetime.datetime):
+			return var.strftime(arg)
+		elif isinstance(var, datetime.date):
+			return var.strftime(arg)
+		elif isinstance(var, float) or isinstance(var, int):
+			return datetime.datetime.fromtimestamp(var).strftime(arg)
+		else:
+			return str(var)
+
+	# all the pre-defined filters
+	_filters = {
+		"pluralize": _pluralize,
+		"yesno": _yesno, 
+		"datetime": _datetime, 
+		"cut": lambda v,a: v.replace(a, ""), 
+		"empty_if_false": lambda v,a: "" if not v else a, 
+		"empty_if_true": lambda v,a: "" if v else a, 
+		"default_if_none": lambda v,a: a if v == None else v, 
+		"lower": lambda v,a: str(v).lower(), 
+		"upper": lambda v,a: str(v).upper(), 
+		"title": lambda v,a: " ".join(s.capitalize() for s in str(v).split(" ")), 
+		"join": lambda v,a: a.join(v), 
+	}
+
+	def __init__(self, locale_dir: typing.Union[Path, str], file_pattern: str, default: str):
+		self.default = default
+		self.locales = Path(locale_dir)
+		self.file_pattern = file_pattern
+
+		if not self.locales.exists():
+			raise FileNotFoundError(f"Locales folder does not exist: {self.locales}")
+		if not self.locales.is_dir():
+			raise NotADirectoryError(f"{self.locales} must be a folder.")
+
+	def filter_exists(self, name: str):
+		return name in self._filters
+
+	def add_filter(self, name: str, function: callable):
+		self._filters[name] = function
+	
+	def remove_filter(self, name: str):
+		del self._filters[name]
+
+	def locale_loaded(self, locale: str):
+		return locale in self._loaded_locales
+
+	def add_locale(self, locale: str, locale_strings: dict):
+		self._loaded_locales[locale] = dict
+	
+	def remove_locale(self, locale: str):
+		del self._loaded_locales[locale]
+
+	def translate(self, node: str, *args, **kwargs):
+		print(f"args: {args}, kwargs: {kwargs}")
+		locale = self.default if not args else args[0]
+		print(f"Using locale {locale}")
+
+		if locale not in self._loaded_locales:
+			lpath = self.locales / self.file_pattern.format(locale=locale)
+			
+			if not lpath.exists():
+				raise FileNotFoundError(f"Locale file {lpath} does not exist!")
+			if not lpath.is_file():
+				raise FileExistsError(f"{self.locales} is not a file!")
+			
+			with lpath.open() as f:
+				# interpret yaml files
+				if lpath.suffix == ".json":
+					self._loaded_locales[locale] = json.loads(f.read())
+				elif lpath.suffix in [".yml", ".yaml"]:
+					self._loaded_locales[locale] = yaml.safe_load(f)
+				else:
+					# TODO: maybe allow different parsers?
+					raise NotImplementedError(f"Loading {lpath.suffix} files are unsupported at this time.")
+		
+		# Split the node string into an array
+		path = node.split('.')
+		unformatted_val = self._loaded_locales[locale]
+		# This should narrow down to a string
+		for p in path:
+			unformatted_val = unformatted_val[p]
+		
+		if not isinstance(unformatted_val, str):
+			raise ValueError(f"the value of {node} must result in a string not {type(unformatted_val)}!")
+
+		# if it's just a normal string translation, return that and don't bother formatting
+		if not "{" in unformatted_val:
+			return unformatted_val
+
+		# Begin parsing all the different values and filters.
+		idx = unformatted_val.find("{")
+		# TODO: support escaping { and }
+		while idx != -1:
+			endpos = unformatted_val.find("}", idx)
+			# make sure exception is correct
+			if endpos == -1:
+				raise SyntaxError("Expected '}' but got EOF, expression begins at column %d" % idx)
+			
+			expression = unformatted_val[idx+1:endpos]
+			replacement = ""
+			variable = func = args = None
+			if "|" in expression:
+				variable, func = expression.split("|", 1)
+				if ":" in func:
+					func, args = func.split(":", 1)
+
+			# Check if our keyword was given or not, otherwise dump a warning and replace with
+			# an empty string.
+			if not variable.lower() in set(k.lower() for k in kwargs.keys()):
+				warnings.warn(f"{variable} has no associated data to be formatted with!", SyntaxWarning)
+			else:
+				# find our key name case insensitively:
+				key = variable.lower()
+				value = None
+				for k, v in kwargs.items():
+					if k.lower() == key:
+						key = k
+						value = v
+						break
+				
+				# If the filter does not exist, show a warning about it and then do a normal
+				# variable formatting replacement
+				if not func in self._filters:
+					# Only log a warning if arguments or a function was expected
+					# Don't warn for simple variable replacements.
+					if not isinstance(func, type(None)) or not isinstance(args, type(None)):
+						warnings.warn(f"\"{func}\" specified but no filter by that name exists!", SyntaxWarning)
+					replacement = value
+				else:
+					replacement = self._filters[func](value, args)
+
+			unformatted_val = unformatted_val[0:idx] + str(replacement) + unformatted_val[endpos+1:]
+
+			# Skip over the just processed formatter
+			idx = unformatted_val.find("{", idx + len(str(replacement)))
+		return unformatted_val
+				
+		
+		
